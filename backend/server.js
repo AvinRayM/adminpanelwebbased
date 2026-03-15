c9b1972f743a44fbade01845a8ad5691e86c5078
AvinRayM <avinrmccammond14@gmail.com>
2026-03-15 18:37:37 -0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
+const express = require("express")
+const cors = require("cors")
 
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+let commands = []
+
+// website sends command
+app.post("/command", (req,res)=>{
+    const cmd = req.body
+    commands.push(cmd)
+    res.json({status:"ok"})
+})
+
+// roblox fetches commands
+app.get("/commands",(req,res)=>{
+    res.json(commands)
+    commands = []
+})
+
+app.listen(3000, ()=>{
+    console.log("Server running")
+})
