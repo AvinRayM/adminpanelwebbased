4588277eb6cfa9225cb143a43c370ead95731eb8
AvinRayM <avinrmccammond14@gmail.com>
2026-03-15 19:05:45 -0500

    Update script.js

--- website/script.js
+++ website/script.js
@@ -21,4 +21,21 @@ alert("Command sent")
 console.error(err)
 })
 
+function sendPrint(){
+
+const msg = document.getElementById("printmsg").value
+
+fetch("https://adminpanelwebbased.onrender.com/command",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+type:"print",
+message:msg
+})
+})
+.then(res => res.json())
+.then(data => console.log(data))
+
 }
